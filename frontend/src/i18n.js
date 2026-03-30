61226d4de7a603dcf791c27480e96a9f4641435d
recaibostanci <recai.bostanci@iyzico.com>
2026-03-30 23:41:17 +0300

    Add i18next-browser-languagedetector for automatic language detection in i18n setup

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,16 +1,17 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import en from './locales/en.json';
 import tr from './locales/tr.json';
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
       en: { translation: en },
       tr: { translation: tr },
     },
-    lng: 'en',
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false,
@@ -18,4 +19,3 @@ i18n
   });
 
 export default i18n;
-
